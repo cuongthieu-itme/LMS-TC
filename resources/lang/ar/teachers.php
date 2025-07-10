ea90ca3c3e4624813d4a1914f5341c328ec57698
Cuong Thieu <cuongtt@solashi.com>
2025-07-10 17:06:25 +0700

    update

--- resources/lang/ar/teachers.php
+++ resources/lang/ar/teachers.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'teachers' => 'Giáo viên',
-    'teacher' => 'Giáo viên',
-    'is_center_manager' => 'Quản lý trung tâm',
-    'return_to_original_admin' => 'Quay lại quản trị viên gốc',
-    'is_examiner' => 'Người kiểm tra',
-    'examiner' => 'Người kiểm tra',
-    'details' => 'Chi tiết',
-    'teachers_count' => 'Số lượng giáo viên',
+  'teachers' => 'Giáo viên',
+  'teacher' => 'Giáo viên',
+  'is_center_manager' => 'Quản lý trung tâm',
+  'return_to_original_admin' => 'Quay lại quản trị viên',
+  'is_examiner' => 'Người kiểm tra',
+  'examiner' => 'Người kiểm tra',
+  'details' => 'Chi tiết',
+  'teachers_count' => 'Số lượng giáo viên',
 ];
